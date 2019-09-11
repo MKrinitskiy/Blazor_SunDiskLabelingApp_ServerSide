7006b9ec1e9382d9a273512693719f281feda824
MKrinitskiy <krinitsky@sail.msk.ru>
2019-09-11 13:30:13 +0000

    draft blueprint for AllSkyImage_SunDisk_Example

---
+++ libs/AllSkyImage_SunDisk_Example.py
@@ -0,0 +1,9 @@
+# TODO: Implements data structure AllSkyImage_SunDisk_Example
+# category=web_API issue=none estimate=3h
+# AllSkyImage_SunDisk_Example should be mirrored with the same-named class in the client-side \n
+# the purpose is to implement the JSON-based exchange mechanics between server-side and client-side \n
+# regarding the labels
+
+class AllSkyImage_SunDisk_Example(object):
+    def __init__(self):
+        pass
