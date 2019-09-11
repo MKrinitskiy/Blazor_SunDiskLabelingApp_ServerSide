6b11d57d4ef9e0b54be61c1c0ff81142a025b30f
MKrinitskiy <krinitsky@sail.msk.ru>
2019-09-11 12:56:52 +0000

    DONE: Implement PreviousImage(app, webapi_client_id, cache_abs_path)

--- handlers_serverside.py
+++ handlers_serverside.py
@@ -45,8 +45,33 @@ def NextImage(app, webapi_client_id, cache_abs_path):
 
 
 def PreviousImage(app, webapi_client_id, cache_abs_path):
-    # TODO: Implement PreviousImage(app, webapi_client_id, cache_abs_path)
+    # DONE: Implement PreviousImage(app, webapi_client_id, cache_abs_path)
     # category=handlers issue=none estimate=2h
     # The mechanics should involve storing all the images that was already processed
     # and PreviousImage() should return them in reversed order
-    return ""
+
+    print('entered PreviousImage()')
+
+    clientHelper = app.clientHelpers[webapi_client_id]
+    print('got clientHelper for the client %s' % webapi_client_id)
+
+    tmp_fname = clientHelper.generated_examples_history.pop()
+    print('tmp_fname = %s' % tmp_fname)
+
+    tmp_base_fname = os.path.basename(tmp_fname)
+    print('tmp_base_fname = %s' % tmp_base_fname)
+
+    img_uri = urljoin('cache', tmp_base_fname)
+
+    response = WebAPI_response(response_code=ResponseCodes.OK,
+                               error=WebAPI_error(),
+                               response_description="previous image sucessfully found")
+    response.StringAttributes['imageURL'] = img_uri
+
+    response.StringAttributes['SunDisk_RoundDataWithUnderlyingImgSize'] = ToJSON(
+        RoundDataWithUnderlyingImgSize(RoundData(512, 512, 100), Size(1920, 1920)))
+    response.StringAttributes['ImgSize_'] = ToJSON(Size(1920, 1920))
+
+    print("sending JSON response:")
+    print('JSON: ' + response.ToJSON())
+    return response.ToJSON()
