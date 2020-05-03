e6e3358f95a745d051f59321e3e17bc5a191d772
Mikhail Krinitskiy <krinitsky@sail.msk.ru>
2020-05-03 17:46:03 +0300

    clientHelpers transfer todo

--- libs/FlaskExtended.py
+++ libs/FlaskExtended.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import g
 
+# TODO: transfer clientHelpers to _AppCtxGlobals
+# category=architecture issue=none estimate=3h
+# If possible, move clientHelpers attribute to the _AppCtxGlobals storage of the Flask app
 
 class FlaskExtended(Flask):
     clientHelpers = dict()
